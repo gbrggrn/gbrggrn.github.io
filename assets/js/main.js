2e7eb1e4bf45b13934fe5e5ff31d17a08b3b91e0
Gustaf Berggren Sörlin <145323927+gbrggrn@users.noreply.github.com>
2026-02-22 20:47:05 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+function toggleTimeline(timelineId) {
+  const wrappers = document.querySelectorAll('.timeline-wrapper');
 
+  wrappers.forEach(wrapper => {
+    wrapper.classList.remove('timeline-visible');
+  });
+
+  const target = docuemtn.getElementById(timelineId);
+  if (target) {
+    target.classList.add('timeline-visible');
+  }
+}
